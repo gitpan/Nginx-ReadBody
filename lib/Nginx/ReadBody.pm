7b0c15b0e00998e0975df7bc256c18f7f6b5740b
Daniel Podolsky <tpaba@cpan.org>
2010-02-16 11:31:25 +0000

    Import of TPABA/Nginx-ReadBody-0.02 from CPAN.
    gitpan-cpan-distribution: Nginx-ReadBody
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         TPABA/Nginx/Nginx-ReadBody-0.02.tar.gz
    gitpan-cpan-author:       TPABA
    gitpan-cpan-maturity:     released

--- lib/Nginx/ReadBody.pm
+++ lib/Nginx/ReadBody.pm
@@ -25,7 +25,7 @@ our @EXPORT = qw(
 	
 );
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use nginx;
 
@@ -115,7 +115,7 @@ Russian doc: L<Nginx::ReadBody:Russian>
 
 Nginx::ReadBody - nginx web server embeded perl module to read and evaluate a request body
 
-I<Version 0.01>
+I<Version 0.02>
 
 =head1 SYNOPSIS
 
